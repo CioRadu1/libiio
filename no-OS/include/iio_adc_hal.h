13d007deaabdfdd6d0d7a1a8a5bb91af6fed9ff5
Radu Ciobanu <Radu-rares.Ciobanu@analog.com>
2026-08-31 15:58:59 +0300

    no-OS: adc: convert HAL to descriptor struct
    Replace the per-board getter functions with a single const iio_adc_hal
    table: fields for the facts, function pointers for init and read_raw.
    
    Signed-off-by: Radu Ciobanu <Radu-rares.Ciobanu@analog.com>

--- no-OS/include/iio_adc_hal.h
+++ no-OS/include/iio_adc_hal.h
@@ -9,11 +9,15 @@
 
 #define IIO_ADC_MAX_CHANNELS 8
 
-int iio_adc_hal_init(void);
-unsigned int iio_adc_hal_num_channels(void);
-const char *iio_adc_hal_channel_id(unsigned int channel);
-int iio_adc_hal_read_raw(unsigned int channel, int *value);
-unsigned int iio_adc_hal_resolution_bits(void);
-int iio_adc_hal_ref_voltage_mv(void);
+struct iio_adc_hal {
+	const char *const *channels;
+	unsigned int       num_channels;
+	unsigned int       resolution_bits;
+	int                ref_voltage_mv;
+	int (*init)(void);
+	int (*read_raw)(unsigned int channel, int *value);
+};
+
+extern const struct iio_adc_hal iio_adc_hal;
 
 #endif /* IIO_ADC_HAL_H */
