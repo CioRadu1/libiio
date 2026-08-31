49da2bca9e6d8f46fd16d7ecb45769c65c0db938
Radu Ciobanu <Radu-rares.Ciobanu@analog.com>
2026-08-31 15:58:59 +0300

    no-OS: iio_adc: Add numeric validation for scale attribute
    Add input validation when writing the scale channel attribute to
    reject non-numeric values. Also add write direction support to the
    backend and refactor platform-specific parameters into parameters.h.
    
    Signed-off-by: Radu Ciobanu <Radu-rares.Ciobanu@analog.com>

--- no-OS/include/iio_device.h
+++ no-OS/include/iio_device.h
@@ -35,22 +35,29 @@ typedef int (*noos_iio_write_attr_t)(void *dev,
 		const char *src, size_t len);
 
 typedef int (*noos_iio_read_samples_t)(void *dev, void *data, size_t bytes);
+typedef int (*noos_iio_write_samples_t)(void *dev, const void *data,
+					size_t bytes);
 
 /**
  * struct noos_iio_device_info - describes one IIO device for the backend
  * @name:          human-readable device name
  * @dev:           pointer to the no-OS device instance (opaque)
+ * @direction:     0 = input (RX), 1 = output (TX)
  * @add_channels:  populate channels on the iio_device (may be NULL)
  * @read_attr:     read an attribute value (may be NULL)
  * @write_attr:    write an attribute value (may be NULL)
+ * @read_samples:  read sample data from hardware (RX, may be NULL)
+ * @write_samples: write sample data to hardware (TX, may be NULL)
  */
 struct noos_iio_device_info {
 	const char *name;
 	void *dev;
+	int direction;
 	noos_iio_add_channels_t add_channels;
 	noos_iio_read_attr_t read_attr;
 	noos_iio_write_attr_t write_attr;
 	noos_iio_read_samples_t read_samples;
+	noos_iio_write_samples_t write_samples;
 };
 
 /**
