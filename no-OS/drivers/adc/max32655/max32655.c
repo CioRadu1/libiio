72ed640054d777c6a7708f6cd4b9ecdd5aa20702
Radu Ciobanu <Radu-rares.Ciobanu@analog.com>
2026-08-31 15:58:59 +0300

    no-OS: adc: convert HAL to descriptor struct
    Replace the per-board getter functions with a single const iio_adc_hal
    table: fields for the facts, function pointers for init and read_raw.
    
    Signed-off-by: Radu Ciobanu <Radu-rares.Ciobanu@analog.com>

--- no-OS/drivers/adc/max32655/max32655.c
+++ no-OS/drivers/adc/max32655/max32655.c
@@ -6,51 +6,27 @@
 
 #include <errno.h>
 #include <stdint.h>
+#include <no_os_util.h>
 #include "iio_adc_hal.h"
 #include "adc.h"
 
-struct adc_channel_map {
-	const char *id;
-	mxc_adc_chsel_t channel;
+static const char *const max32655_channels[] = {
+	"voltage0",
 };
 
-static const struct adc_channel_map channel_map[] = {
-	{"voltage0", MXC_ADC_CH_0},
-};
-
-#define NUM_CHANNELS (sizeof(channel_map) / sizeof(channel_map[0]))
-
-unsigned int iio_adc_hal_num_channels(void)
+static int max32655_adc_init(void)
 {
-	return NUM_CHANNELS;
+	return MXC_ADC_Init();
 }
 
-const char *iio_adc_hal_channel_id(unsigned int channel)
-{
-	if (channel >= NUM_CHANNELS)
-		return NULL;
-
-	return channel_map[channel].id;
-}
-
-unsigned int iio_adc_hal_resolution_bits(void)
-{
-	return 10;
-}
-
-int iio_adc_hal_ref_voltage_mv(void)
-{
-	return 1220;
-}
-
-int iio_adc_hal_read_raw(unsigned int channel, int *value)
+static int max32655_adc_read_raw(unsigned int channel, int *value)
 {
 	int raw;
 
-	if (channel >= NUM_CHANNELS)
+	if (channel >= NO_OS_ARRAY_SIZE(max32655_channels))
 		return -EINVAL;
 
-	raw = MXC_ADC_StartConversion(channel_map[channel].channel);
+	raw = MXC_ADC_StartConversion(MXC_ADC_CH_0 + channel);
 	if (raw < 0)
 		return raw;
 
@@ -59,7 +35,11 @@ int iio_adc_hal_read_raw(unsigned int channel, int *value)
 	return 0;
 }
 
-int iio_adc_hal_init(void)
-{
-	return MXC_ADC_Init();
-}
+const struct iio_adc_hal iio_adc_hal = {
+	.channels        = max32655_channels,
+	.num_channels    = NO_OS_ARRAY_SIZE(max32655_channels),
+	.resolution_bits = 10,
+	.ref_voltage_mv  = 1220,
+	.init            = max32655_adc_init,
+	.read_raw        = max32655_adc_read_raw,
+};
--- no-OS/drivers/adc/max32690/max32690.c
+++ no-OS/drivers/adc/max32690/max32690.c
@@ -6,47 +6,31 @@
 
 #include <errno.h>
 #include <stdint.h>
+#include <no_os_util.h>
 #include "iio_adc_hal.h"
 #include "adc.h"
 
-struct adc_channel_map {
-	const char *id;
-	mxc_adc_chsel_t channel;
-	bool is_temp;
-};
-
-static const struct adc_channel_map channel_map[] = {
-	{"voltage0", MXC_ADC_CH_0, false},
-};
-
-#define NUM_CHANNELS (sizeof(channel_map) / sizeof(channel_map[0]))
-
 #define ADC_POLL_TIMEOUT 1000000
 
-unsigned int iio_adc_hal_num_channels(void)
+static const char *const max32690_channels[] = {
+	"voltage0",
+};
+
+static int max32690_adc_init(void)
 {
-	return NUM_CHANNELS;
+	mxc_adc_req_t adc_cfg = {
+		.clock = MXC_ADC_CLK_IBRO,
+		.clkdiv = MXC_ADC_CLKDIV_4,
+		.cal = MXC_ADC_SKIP_CAL,
+		.ref = MXC_ADC_REF_INT_1V25,
+		.trackCount = 4,
+		.idleCount = 17,
+	};
+
+	return MXC_ADC_Init(&adc_cfg);
 }
 
-const char *iio_adc_hal_channel_id(unsigned int channel)
-{
-	if (channel >= NUM_CHANNELS)
-		return NULL;
-
-	return channel_map[channel].id;
-}
-
-unsigned int iio_adc_hal_resolution_bits(void)
-{
-	return 12;
-}
-
-int iio_adc_hal_ref_voltage_mv(void)
-{
-	return 1250;
-}
-
-int iio_adc_hal_read_raw(unsigned int channel, int *value)
+static int max32690_adc_read_raw(unsigned int channel, int *value)
 {
 	mxc_adc_slot_req_t slot_req;
 	mxc_adc_conversion_req_t conv_req = {
@@ -58,17 +42,12 @@ int iio_adc_hal_read_raw(unsigned int channel, int *value)
 		.num_slots = 1,
 	};
 	volatile uint32_t timeout;
-	bool is_temp;
 	int ret;
 
-	if (channel >= NUM_CHANNELS)
+	if (channel >= NO_OS_ARRAY_SIZE(max32690_channels))
 		return -EINVAL;
 
-	slot_req.channel = channel_map[channel].channel;
-	is_temp = channel_map[channel].is_temp;
-
-	if (is_temp)
-		MXC_ADC_TS_SelectEnable();
+	slot_req.channel = MXC_ADC_CH_0 + channel;
 
 	MXC_ADC_Clear_ChannelSelect();
 
@@ -104,22 +83,14 @@ out_disable:
 	MXC_ADC_DisableConversion();
 
 out:
-	if (is_temp)
-		MXC_ADC_TS_SelectDisable();
-
 	return ret;
 }
 
-int iio_adc_hal_init(void)
-{
-	mxc_adc_req_t adc_cfg = {
-		.clock = MXC_ADC_CLK_IBRO,
-		.clkdiv = MXC_ADC_CLKDIV_4,
-		.cal = MXC_ADC_SKIP_CAL,
-		.ref = MXC_ADC_REF_INT_1V25,
-		.trackCount = 4,
-		.idleCount = 17,
-	};
-
-	return MXC_ADC_Init(&adc_cfg);
-}
+const struct iio_adc_hal iio_adc_hal = {
+	.channels        = max32690_channels,
+	.num_channels    = NO_OS_ARRAY_SIZE(max32690_channels),
+	.resolution_bits = 12,
+	.ref_voltage_mv  = 1250,
+	.init            = max32690_adc_init,
+	.read_raw        = max32690_adc_read_raw,
+};
